a33d0fc37cc46c79b8a87ddf799ca7143bb2184f
lab-811 <Alisher>
2021-11-25 19:39:11 +0600

    kafka producer

--- src/main/java/com/example/movierating/MovieRatingApplication.java
+++ src/main/java/com/example/movierating/MovieRatingApplication.java
@@ -1,8 +1,20 @@
 package com.example.movierating;
 
+import com.example.movierating.model.Rating;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -12,4 +24,17 @@ public class MovieRatingApplication {
         SpringApplication.run(MovieRatingApplication.class, args);
     }
 
+    @Bean
+    public KafkaTemplate<String, Optional<Rating>> myMessageKafkaTemplate() {
+        return new KafkaTemplate<>(greetingProducerFactory());
+    }
+
+    @Bean
+    public ProducerFactory<String, Optional<Rating>> greetingProducerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
 }
--- src/main/java/com/example/movierating/controller/MovieRatingController.java
+++ src/main/java/com/example/movierating/controller/MovieRatingController.java
@@ -4,6 +4,7 @@ import com.example.movierating.model.Rating;
 import com.example.movierating.service.MovieRatingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,13 +20,22 @@ public class MovieRatingController {
     @Autowired
     private MovieRatingService service;
 
+    private static final String TOPIC = "movie_rating";
+
+    @Autowired
+    private KafkaTemplate<String, Optional<Rating>> kafkaTemplate;
+
     @GetMapping
     public List<Rating> getRatings() {
         return  service.getRatings();
     }
 
+
+
     @GetMapping("/{movieId}")
     public Optional<Rating> getMovieRating(@PathVariable("movieId") Long movieId) {
+
+        kafkaTemplate.send(TOPIC, service.getMovieRating(movieId));
         return  service.getMovieRating(movieId);
     }
 
